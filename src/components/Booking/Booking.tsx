7dade2791b99aa6135c5732f540f16f732d055b7
Zelalem <mekonnen.zelalem251@gmail.com>
2022-11-23 21:34:31 +0300

    Completed implementation of the booking section

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -35,6 +35,36 @@ const Booking = () => {
 									Email address
 								</label>
 							</div>
+
+							<div className='form__group u_margin_bottom_medium'>
+								<div className='form__radio-group'>
+									<input
+										type='radio'
+										className='form__radio-input'
+										id='small'
+										name='size'
+									/>
+									<label htmlFor='small' className='form__radio-label'>
+										<span className='form__radio-button'></span>
+										Small tour group
+									</label>
+								</div>
+								<div className='form__radio-group'>
+									<input
+										type='radio'
+										className='form__radio-input'
+										id='large'
+										name='size'
+									/>
+									<label htmlFor='large' className='form__radio-label'>
+										<span className='form__radio-button'></span>
+										Large tour group
+									</label>
+								</div>
+							</div>
+							<div className='form__group'>
+								<button className='btn btn--green'>Next step &rarr;</button>
+							</div>
 						</form>
 					</div>
 				</div>
